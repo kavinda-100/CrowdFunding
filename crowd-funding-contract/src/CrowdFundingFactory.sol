7a5d7263bfb9d20510ad636045e3e4b737b01bd3
kavinda-100 <kavindarathnayake100@gmail.com>
2025-08-09 12:22:14 +0530

    feat: Add error handling and event emission for campaign creation in CrowdFundingFactory

--- crowd-funding-contract/src/CrowdFundingFactory.sol
+++ crowd-funding-contract/src/CrowdFundingFactory.sol
@@ -11,6 +11,12 @@ import {CrowdFunding} from "./CrowdFunding.sol";
  * @notice This contract is responsible for creating and managing crowd funding campaigns.
  */
 contract CrowdFundingFactory is Ownable {
+    // ------------------------------ Errors ------------------------------
+    error CrowdFundingFactory__InvalidAddress();
+
+    // -------------------------- Events --------------------------
+    event CampaignCreated(address indexed campaignAddress, address indexed owner, string campaignName);
+
     // -------------------------- State Variables --------------------------
     struct Campaign {
         address campaignAddress;
@@ -22,12 +28,33 @@ contract CrowdFundingFactory is Ownable {
     Campaign[] private campaigns; // Array to store all campaigns
     mapping(address => Campaign[]) private userCampaigns; // Mapping from user address to their campaigns
 
+    // -------------------------- Modifiers --------------------------
+
+    /**
+     * @dev Modifier to check if an address is valid (not zero).
+     */
+    modifier IsValidAddress(address _address) {
+        if (_address == address(0)) {
+            revert CrowdFundingFactory__InvalidAddress();
+        }
+        _;
+    }
+
     // -------------------------- Constructor --------------------------
     constructor() Ownable(msg.sender) {}
 
+    // -------------------------- Public/External Functions --------------------------
+
+    /**
+     * @dev Creates a new crowdfunding campaign.
+     * @param _name The name of the campaign.
+     * @param _description The description of the campaign.
+     * @param _goal The funding goal for the campaign.
+     * @param _deadline The deadline for the campaign.
+     */
     function createCampaign(string memory _name, string memory _description, uint256 _goal, uint256 _deadline)
         external
-        returns (address)
+        IsValidAddress(msg.sender)
     {
         // deploy the CrowdFunding contract
         CrowdFunding campaign = new CrowdFunding(_name, _description, _goal, _deadline, msg.sender);
@@ -43,6 +70,23 @@ contract CrowdFundingFactory is Ownable {
         campaigns.push(newCampaign);
         userCampaigns[msg.sender].push(newCampaign);
 
-        return address(campaign);
+        // Emit the CampaignCreated event
+        emit CampaignCreated(address(campaign), msg.sender, _name);
+    }
+
+    // -------------------------- View Functions --------------------------
+
+    /**
+     * @dev Returns all campaigns.
+     */
+    function getAllCampaigns() external view returns (Campaign[] memory) {
+        return campaigns;
+    }
+
+    /**
+     * @dev Returns all campaigns created by a user.
+     */
+    function getUserCampaigns(address _user) external view returns (Campaign[] memory) {
+        return userCampaigns[_user];
     }
 }
