b8e93179758435504140552b7a99043fa1909fa8
kavinda-100 <kavindarathnayake100@gmail.com>
2025-08-09 12:16:14 +0530

    feat: Refactor CrowdFundingFactory to include campaign struct and user campaign mapping

--- crowd-funding-contract/src/CrowdFundingFactory.sol
+++ crowd-funding-contract/src/CrowdFundingFactory.sol
@@ -11,14 +11,38 @@ import {CrowdFunding} from "./CrowdFunding.sol";
  * @notice This contract is responsible for creating and managing crowd funding campaigns.
  */
 contract CrowdFundingFactory is Ownable {
+    // -------------------------- State Variables --------------------------
+    struct Campaign {
+        address campaignAddress;
+        address owner;
+        string campaignName;
+        uint256 createdAt;
+    } // Campaign struct
+
+    Campaign[] private campaigns; // Array to store all campaigns
+    mapping(address => Campaign[]) private userCampaigns; // Mapping from user address to their campaigns
+
+    // -------------------------- Constructor --------------------------
     constructor() Ownable(msg.sender) {}
 
     function createCampaign(string memory _name, string memory _description, uint256 _goal, uint256 _deadline)
         external
-        onlyOwner
         returns (address)
     {
+        // deploy the CrowdFunding contract
         CrowdFunding campaign = new CrowdFunding(_name, _description, _goal, _deadline, msg.sender);
+
+        Campaign memory newCampaign = Campaign({
+            campaignAddress: address(campaign),
+            owner: msg.sender,
+            campaignName: _name,
+            createdAt: block.timestamp
+        });
+
+        // save the campaign details
+        campaigns.push(newCampaign);
+        userCampaigns[msg.sender].push(newCampaign);
+
         return address(campaign);
     }
 }
