0ccae49d35be358d78536ab109b0b58a0bf2117a
kavinda-100 <kavindarathnayake100@gmail.com>
2025-08-10 09:32:48 +0530

    feat: Add user setup and modifier for deploying CrowdFunding contracts in tests

--- crowd-funding-contract/test/unit/CrowdFundingTest.t.sol
+++ crowd-funding-contract/test/unit/CrowdFundingTest.t.sol
@@ -9,6 +9,10 @@ contract CrowdFundingTest is Test {
     CrowdFunding crowdFundingContract;
 
     address public owner = makeAddr("owner");
+    address public user1 = makeAddr("user1");
+    address public user2 = makeAddr("user2");
+
+    uint256 public usersInitialBalance = 10 ether;
 
     function setUp() public {
         crowdFundingContract = new CrowdFunding({
@@ -18,10 +22,39 @@ contract CrowdFundingTest is Test {
             _deadline: 1,
             _owner: owner
         });
+
+        // Set initial balances for users
+        vm.deal(user1, usersInitialBalance);
+        vm.deal(user2, usersInitialBalance);
+    }
+
+    // --------------------------- Modifier for tests ---------------------------
+
+    /**
+     * @param _owner The address of the owner of the campaign.
+     * @notice This modifier deploys a new CrowdFunding contract instance.
+     */
+    modifier DeployCrowdFundingContract(address _owner) {
+        vm.startPrank(_owner);
+
+        // Create a new CrowdFunding contract instance
+        crowdFundingContract = new CrowdFunding({
+            _name: "TestCampaign",
+            _description: "This is a test campaign for CrowdFunding",
+            _goal: 100 wei,
+            _deadline: 1,
+            _owner: _owner
+        });
+
+        vm.stopPrank();
+        _;
     }
 
     // --------------------------- CrowdFunding contract creation tests ---------------------------
 
+    /**
+     * @notice This function tests the creation of a new CrowdFunding contract.
+     */
     function test_createContract() public {
         // create an instance of the CrowdFunding contract
         CrowdFunding newCampaign = new CrowdFunding({
