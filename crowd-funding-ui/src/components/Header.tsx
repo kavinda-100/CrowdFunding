43428af371004d266469944b0c6fbd6b28e03833
kavinda-100 <kavindarathnayake100@gmail.com>
2025-08-11 10:23:11 +0530

    feat: update Header component to include logo and navigation links

--- crowd-funding-ui/src/components/Header.tsx
+++ crowd-funding-ui/src/components/Header.tsx
@@ -3,11 +3,27 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import React from "react";
 import { ModeToggle } from "./ModeToggle";
+import Link from "next/link";
+import { useAccount } from "wagmi";
+import Image from "next/image";
 
 const Header = () => {
+  const { isConnected, address } = useAccount();
   return (
     <header className="flex size-full items-center justify-between px-4 py-2">
-      <h1 className="text-lg font-bold">🏦 Crowd Funding</h1>
+      <div className="flex items-center justify-center gap-5">
+        <Link href="/">
+          <Image
+            src="/logo.svg"
+            alt="Crowd Funding Logo"
+            width={40}
+            height={40}
+            className="dark:invert-100"
+          />
+        </Link>
+        {isConnected && <Link href={`/dashboard/${address}`}>Dashboard</Link>}
+        <Link href="/about">About</Link>
+      </div>
 
       <nav className="flex items-center gap-3">
         <ConnectButton
