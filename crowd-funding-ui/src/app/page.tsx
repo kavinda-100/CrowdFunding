88888a3b05dddf176b9d2781d09acd496975ca2f
kavinda-100 <kavindarathnayake100@gmail.com>
2025-08-11 11:07:05 +0530

    feat: create HeroSection component and update HomePage layout for improved structure

--- crowd-funding-ui/src/app/page.tsx
+++ crowd-funding-ui/src/app/page.tsx
@@ -1,12 +1,10 @@
+import HeroSection from "@/components/sections/HeroSection";
+
 export default function HomePage() {
   return (
-    <section className="size-full">
-      <h2 className="text-3xl font-bold">
-        Welcome to the Crowd Funding Platform
-      </h2>
-      <p className="mt-4 text-lg">
-        Here you can create and manage your crowdfunding campaigns.
-      </p>
+    <section className="flex size-full flex-col gap-5">
+      {/* Hero section */}
+      <HeroSection />
     </section>
   );
 }
