4b6ad699363af6f02f7504cc33f9337e1fe38072
kavinda-100 <kavindarathnayake100@gmail.com>
2025-08-11 12:35:48 +0530

    feat: update environment variables for Anvil deployment

--- crowd-funding-ui/src/constant/index.ts
+++ crowd-funding-ui/src/constant/index.ts
@@ -1,2 +1,7 @@
-export const PRIVATE_KEY = "<YOUR_PRIVATE_KEY>";
-export const FACTORY_CONTRACT_ADDRESS = "<YOUR_CONTRACT_ADDRESS>";
+// anvil private key (#1)
+export const PRIVATE_KEY =
+  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+
+// anvil local deployed one
+export const FACTORY_CONTRACT_ADDRESS =
+  "0x5FbDB2315678afecb367f032d93F642f64180aa3";
